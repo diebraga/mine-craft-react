12f5927f121d90a47f8b10d3edee5866af382784
Diego Braga <diegobraga@hertz.com>
2022-09-29 20:58:00 +0100

    transparent glass box

--- src/components/Cube/index.tsx
+++ src/components/Cube/index.tsx
@@ -1,44 +1,52 @@
-import { useBox } from '@react-three/cannon';
-import React from 'react';
-import { useStore } from '../../hooks/useStore';
-import { handleAddCubeToCube } from '../../utils/handleAddCubeToCube';
-import * as textures from '../textures'
+import { useBox } from "@react-three/cannon";
+import React, { useState } from "react";
+import { useStore } from "../../hooks/useStore";
+import { handleAddCubeToCube } from "../../utils/handleAddCubeToCube";
+import * as textures from "../textures";
 
 type Props = {
-  position: [x: number, y: number, z: number]
-  texture: string
-}
+  position: [x: number, y: number, z: number];
+  texture: string;
+};
 
 const Cube: React.FC<Props> = ({ position, texture }) => {
   const [ref] = useBox(() => ({
     type: "Static",
-    position
-  }))
-  const [addCube, removeCube] = useStore((state) => [state.addCube, state.removeCube])
+    position,
+  }));
+  const [addCube, removeCube] = useStore((state) => [
+    state.addCube,
+    state.removeCube,
+  ]);
 
   // @ts-ignore
-  const activeTexture = textures[texture + "Texture"]
+  const activeTexture = textures[texture + "Texture"];
 
   return (
     <mesh
       ref={ref}
       onClick={(e) => {
-        e.stopPropagation()
+        e.stopPropagation();
         // @ts-ignore
-        const clickedFace = Math.floor(e.faceIndex / 2)
-        const { x, y, z } = ref.current.position
+        const clickedFace = Math.floor(e.faceIndex / 2);
+        const { x, y, z } = ref.current.position;
         // @ts-ignore
         if (e.shiftKey) {
-          removeCube(x, y, z)
-          return
+          removeCube(x, y, z);
+          return;
         }
-        handleAddCubeToCube(clickedFace, addCube, x, y, z)
+        handleAddCubeToCube(clickedFace, addCube, x, y, z);
       }}
     >
       <boxGeometry attach="geometry" />
-      <meshStandardMaterial map={activeTexture} attach="material" />
+      <meshStandardMaterial
+        map={activeTexture}
+        attach="material"
+        transparent={true}
+        opacity={texture === "glass" ? 0.6 : 1}
+      />
     </mesh>
   );
-}
+};
 
 export { Cube };
