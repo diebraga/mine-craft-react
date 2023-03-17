93693b0d6e1cb90660f05f6a05eb7d706a85b2fc
Diego Braga <diegobraga@hertz.com>
2023-03-17 11:15:51 +0000

    texture selector

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,6 @@ function App() {
       </Box>
       <Menu />
       <TextureSelector />
-      <Controller />
     </>
   );
 }
--- src/components/TextureSelector/index.tsx
+++ src/components/TextureSelector/index.tsx
@@ -1,6 +1,20 @@
+import { Flex, Image } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { useKeyboard } from "../../hooks/useKeyBoard";
 import { useStore } from "../../hooks/useStore";
+import dirtImage from "../../assets/dirt.jpeg";
+import glassImage from "../../assets/glass.jpeg";
+import grassImage from "../../assets/grass.jpeg";
+import logImage from "../../assets/log.jpeg";
+import woodImage from "../../assets/wood.jpeg";
+
+const texturesImages = [
+  { name: "dirt", src: dirtImage },
+  { name: "glass", src: glassImage },
+  { name: "grass", src: grassImage },
+  { name: "log", src: logImage },
+  { name: "wood", src: woodImage },
+];
 
 const TextureSelector: React.FC = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -10,7 +24,7 @@ const TextureSelector: React.FC = () => {
   ]);
   // console.log(activeTexture);
   const { glass, dirt, grass, log, wood } = useKeyboard();
-
+  // console.log(activeTexture);
   const textures = { glass, dirt, grass, log, wood };
   useEffect(() => {
     const pressedTexture = Object.entries(textures).find(([k, v]) => v);
@@ -30,7 +44,35 @@ const TextureSelector: React.FC = () => {
   }, []);
 
   return (
-    <>{isVisible && <div className="textureSelector">Texture Selector</div>}</>
+    <Flex
+      flexDir="column"
+      maxW="200px"
+      bottom="10"
+      left="10"
+      position="absolute"
+      cursor="pointer"
+      bg="whiteAlpha.800"
+      borderRadius="full"
+      py="4"
+      px="3"
+      zIndex={1000}
+    >
+      {Object.entries(textures).map(([name, texture]) => {
+        return (
+          <Image
+            key={name}
+            src={texturesImages.find((item) => item.name === name)?.src}
+            w="50px"
+            h="50px"
+            rounded="full"
+            mb="2"
+            border={activeTexture === name ? "4px solid red" : ""}
+            cursor="pointer"
+            onClick={() => setTexture(name)}
+          />
+        );
+      })}
+    </Flex>
   );
 };
 
